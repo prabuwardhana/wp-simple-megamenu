61d3977875b1a8731d17a130b29805e4942f86b0
Dian Nandiwardhana <prabuwardhana03@yahoo.com>
2022-01-29 23:49:07 +0900

    add plugin header

---
+++ simple-megamenu.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Plugin Name: Simple MegaMenu
+ * Description: Turn your Wordpress navigation menu into a megamenu in simple steps
+ * Version:     0.1.0
+ * Author:      Dian Nandiwardhana
+ * Author URI:  https://github.com/prabuwardhana
+ * Text Domain: simple-megamenu
+ * License:     GPLv3
+ * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ **/
