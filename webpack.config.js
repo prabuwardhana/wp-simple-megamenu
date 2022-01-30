927ad66c306df45eb2693e600fb8f956d66a5140
Dian Nandiwardhana <prabuwardhana03@yahoo.com>
2022-01-30 11:50:16 +0900

    add clean webpack plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const TerserPlugin = require("terser-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
   entry: path.resolve(__dirname, "src/js/megamenu.js"),
@@ -42,6 +43,7 @@ module.exports = {
     ],
   },
   plugins: [
+    new CleanWebpackPlugin({}),
     new MiniCssExtractPlugin({
       filename: "./css/[name].css",
     }),
