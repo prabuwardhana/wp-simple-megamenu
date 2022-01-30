4a97820cc7f9a0239985e4584bc1a15c0dc4bee1
Dian Nandiwardhana <prabuwardhana03@yahoo.com>
2022-01-30 19:55:42 +0900

    require plugin customizer class

--- simple-megamenu.php
+++ simple-megamenu.php
@@ -17,6 +17,7 @@ define('SMM_PATH', plugin_dir_path(__FILE__));
 define('SMM_URL', plugin_dir_url(__FILE__));
 
 require_once SMM_PATH . '/inc/walkers/Walker_Nav_Megamenu.php';
+require_once SMM_PATH . '/inc/customizer/class-customizer.php';
 require_once SMM_PATH . '/inc/class-admin.php';
 require_once SMM_PATH . '/inc/class-plugin-init.php';
 require_once SMM_PATH . '/inc/class-menu-fields.php';
